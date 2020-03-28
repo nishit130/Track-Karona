0c15ce9ded35e4e89d949d56f9a18db725bfacd2
manan <mrpandya2017@gmail.com>
2020-03-28 18:56:24 +0530

    added precautions and symptoms

--- covid.c
+++ covid.c
@@ -28,10 +28,10 @@ int top10(){
     }
     // printf("the final line is --> %s\n",a); //test for finding line containing <tbody>
     printf("\n\nTOP 10 COUNTRIES : \n\n");
-    printf("|sr| country\t| total cases\t| new cases\t| total deaths\t| new deaths |\n");
+    printf("|sr|\tcountry\t\t| total cases | new cases | total deaths | new deaths |\n");
     int c=1;
     while(c<=10){
-        printf("--------------------------------------------------------------------------------\n");
+        // printf("--------------------------------------------------------------------------------\n");
         // printf("%d\n",c);
         fgets(a,500,fp1); //for <tr>
         fgets(a,500,fp1); //for countryname
@@ -41,7 +41,7 @@ int top10(){
         while(a[x]!='<'){
             top[c].name[y++]=a[x++];
         }
-        printf("|%d | %s \t",(c),top[c].name);//test to print the country's name
+        printf(" %2d   %-20s",(c),top[c].name);//test to print the country's name
         // printf("--------------------------------------------------------------------------------\n");
 
         //for t_cases
@@ -54,7 +54,7 @@ int top10(){
                 }
             top[c].t_case[ytc++]=a[xtc++];
         }
-        printf("| %s \t",top[c].t_case);// test to print t_case
+        printf("%-13s ",top[c].t_case);// test to print t_case
 
         //for n_case
         fgets(a,500,fp1);
@@ -66,7 +66,7 @@ int top10(){
             }
             top[c].n_case[ync++]=a[xnc++];
         }
-        printf("| %s \t",top[c].n_case);//test to print n_case
+        printf("  %-11s ",top[c].n_case);//test to print n_case
 
         //for t_deaths
         fgets(a,500,fp1);
@@ -78,7 +78,7 @@ int top10(){
             }
             top[c].t_deaths[ytd++]=a[xtd++];
         }
-        printf("| %s \t",top[c].t_deaths);//test to print t_deaths
+        printf("  %-14s \t",top[c].t_deaths);//test to print t_deaths
 
         //for n_deaths
         fgets(a,500,fp1);
@@ -90,7 +90,7 @@ int top10(){
             }
             top[c].n_deaths[ynd++]=a[xnd++];
         }
-        printf("| %s       |\n",top[c].n_deaths);//test to print n_deaths
+        printf("  %-12s\n",top[c].n_deaths);//test to print n_deaths
 
         //for skipping the next few lines whos data isnt needed
         // fgets(a,500,fp1);
@@ -101,7 +101,7 @@ int top10(){
             }
         }
         c++;
-        printf("--------------------------------------------------------------------------------\n");
+        // printf("--------------------------------------------------------------------------------\n");
         // printf("\n");
     }
     fclose(fp1);  
@@ -114,38 +114,26 @@ int ind_data(){
     char a[500];
     fgets(a,500,fp1);
     // puts(a);
-    int q=0;
+    char q[50]="<tbody>";
     //skipping to the line where india will be found
-    int w=0;
-    while(w++<1084){
-        fgets(a,500,fp1);
+    char w[50]="India";
+    while(fgets(a,500,fp1)!=NULL){
+        if(strstr(a,q)!=NULL){
+            break;
+        }
     }
-    // printf("the final line is --> %s\n",a); //test
-    int p1=0;//pointer for india start
-    int fc='1';//pointer of the cursor initialized to a random value
-    while(fc++!=EOF){
-        fc=fgetc(fp1);
-        fseek(fp1,-2,SEEK_CUR);
-        fgets(a,500,fp1);//<tr>
-        fgets(a,500,fp1);//country name
+    while(fgets(a,500,fp1)!=NULL){
+        if(strstr(a,w)!=NULL){
+            break;
+        }
+    }
         int p=0;
         while(a[p++]!='>');//<td>
-        //if theres no link for india
-            if(a[p]=='I'&&a[p+1]=='n'&&a[p+2]=='d'&&a[p+3]=='i'&&a[p+4]=='a'){
-                p1=p;
-                break;
-            }
-        while(a[p++]!='>');//a
-          // in case a link is added
-            if(a[p]=='I'&&a[p+1]=='n'&&a[p+2]=='d'&&a[p+3]=='i'&&a[p+4]=='a'){
-                p1=p;
-                break;
-            }
-    }
+        while(a[p++]!='>');//<a href>
     // printf("the final line is --> %s\n",a); //test
     int xi=0;
-    while(a[p1]!='<'){
-        ind.name[xi++]=a[p1++];
+    while(a[p]!='<'){
+        ind.name[xi++]=a[p++];
     }
     printf("--------------------------------------------------------------------------------\n");
     printf("%s \n",ind.name);
@@ -187,13 +175,42 @@ int ind_data(){
 }
 int print_menu(){
     int menu;
-    printf("\n\nWELCOME TO TRACK-KARUNA\n");
+    printf("\n\nWELCOME TO TRACK-KARONA\n");
     printf("--------------------------------------------------------------------------------\n");
     printf("\t press : \n");
-    printf("1. GLOBAL STATUS\n2. INDIA'S STATUS\n3. QUIT\n");
+    printf("1. GLOBAL STATUS\n2. INDIA'S STATUS\n3. SYMPTOMS\n4. PRECAUTIONS\n5. QUIT\n");
     scanf("%d ",&menu);
     printf("--------------------------------------------------------------------------------\n");
     return menu;
+}
+void print_sym(){
+    printf("\n\n \t SYMPTOMS : \n\n");
+    printf("--------------------------------------------------------------------------------\n");
+    printf("  People may be sick with the virus for 1 to 14 days before developing symptoms. The most common symptoms of coronavirus disease (COVID-19) are fever, tiredness, and dry cough. Most people (about 80%) recover from the disease without needing special treatment.\n");
+    printf("  More rarely, the disease can be serious and even fatal. Older people, and people with other medical conditions (such as asthma, diabetes, or heart disease), may be more vulnerable to becoming severely ill.\n");
+    printf("  People may experince:\n");
+    printf("    1. Cough\n");
+    printf("    2. Fever\n");
+    printf("    3. Tiredness\n");
+    printf("    4. Difficulty in breathing (severe cases)\n\n");
+    printf("--------------------------------------------------------------------------------\n");
+}
+void print_info(){
+    printf("\n\n\t PRECAUTIONS : \n\n");
+    printf("--------------------------------------------------------------------------------\n");
+    printf("1. Wash your hands properly: \n");
+    printf("\t Washing your hands with soap and water or using alcohol based hand rub kills viruses that may be on your hands.\n");
+    printf("2. Maintain social distancing (atleast 1m ie 3 ft):\n");
+    printf("\t When someone coughs or sneezes they spray droplets from their nose or mouth which may contain virus. If you're to close, you can breathe in the droplets, including the COVID-19 virus if the coughing person has the disease.\n");
+    printf("3. Avoid touching eyes, nose and mouth:\n");
+    printf("\t Hands touch many surfaces and can pickup viruses. Once contaminated, hands can transfer the virus to the eyes, nose your mouth. From there, the virus can enter your body and can make you sick.\n");
+    printf("4. Practice respiratory hygiene:\n");
+    printf("\t Droplets spread virus. By following good respiratory hygiene like covering your mouth and nose while sneezing and coughing you protect the people around you from viruses like cold, flu, COVID-19.\n");
+    printf("5. If you have fever, cough and difficulty breatthing, seek medical care early:\n");
+    printf("\t National and local authorities will have the most up to date information on the situtation in  your area. Calling in advance will allow your health care provider to quickly direct you to the right health facility. this will also protect you and help prevent the spread of this viruses and other infections.\n");
+    printf("6. Stay informed and follow advice given by your healthcare provider:\n");
+    printf("\t National and local authorities will have up to date information on whether COVID-19 is spreading in your area. They are the best placed to advise on what people in your area should be doing to protect themselves.\n\n");
+    printf("--------------------------------------------------------------------------------\n");
 }
 int main(){
     //file download 
@@ -211,20 +228,27 @@ int main(){
         printf("\n");
     }
     else{
-        printf("error %s \n",curl_easy_strerror(result));
+        printf("error %s \n Check your internet connection\nOptins 1 & 2 unavailable\n",curl_easy_strerror(result));
     }
     fclose(fp);
     curl_easy_cleanup(curl);
     int menu;
-    while(menu!=3){
+    while(menu!=5){
         menu=print_menu();
     switch(menu){
         case 1: top10();
             break;
         case 2: ind_data();
             break;
+        case 3: print_sym();
+            break;
+        case 4: print_info();
+            break;   
     }
     }
+    printf("--------------------------------------------------------------------------------\n");
+    printf("THANK YOU\n");
+    printf("--------------------------------------------------------------------------------\n");
     remove("data.html"); //to delete the file
     return 0;
 }
