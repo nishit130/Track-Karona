8257b3f9a24ee5fae5edb3388153c8531ea249e3
Nishit Patel <nishitlimbani130@gmail.com>
2020-04-30 15:51:30 +0530

    new approach

--- covid.c
+++ covid.c
@@ -1,184 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <jansson.h>
 #include<curl/curl.h>
-#include<stdlib.h>
-#include<stdio.h>
-#include<string.h>
-#include<math.h>
-#include<string.h>
-#define BUFFER_SIZE 1000
-struct data{
-    char name[20];
-    char t_case[6];
-    char n_case[5];
-    char t_deaths[5];
-    char n_deaths[4];
+
+struct data_all_country{
+	char country_name[100];
+	int total_confirmed;
+	int new_confirmed;
+	int total_deaths;
+	int new_deaths;
+	int total_recovered;
+	int new_recovered;
 };
-struct data top[10];
-struct data ind;
-int top10(){
-     //data extraction
-    FILE * fp1;
-    fp1=fopen("data.html","rb");
-    char a[500];
-    // puts(a); //test
-    char tbody[505]="<td style=\"display:none\" data-continent=\"all\">All</td>";
-    while(fgets(a,500,fp1)!=NULL){
-        if(strstr(a,tbody)!=NULL){
-            break;
-        }
-    }
-    // printf("the final line is --> %s\n",a); //test for finding line containing <tbody>
-    printf("\n\nTOP 10 COUNTRIES : \n\n");
-    printf("|sr|\tcountry\t\t| total cases | new cases | total deaths | new deaths |\n");
-    int c=1;
-    fgets(a,500,fp1);
-    while(c<=10){
-        // printf("--------------------------------------------------------------------------------\n");
-        // printf("%d\n",c);
-        fgets(a,500,fp1); //for <tr>
-        fgets(a,500,fp1); //for countryname
-        int x=0,y=0;
-        while(a[x++]!='>');
-        while(a[x++]!='>');
-        while(a[x]!='<'){
-            top[c].name[y++]=a[x++];
-        }
-	if(c!=0)
-        	printf(" %2d   %-20s",(c),top[c].name);//test to print the country's name
-        // printf("--------------------------------------------------------------------------------\n");
-
-        //for t_cases
-        fgets(a,500,fp1);
-        int xtc=0,ytc=0;
-        while(a[xtc++]!='>');
-        while(a[xtc]!='<'){
-            if(a[xtc]==' '){
-                break;
-                }
-            top[c].t_case[ytc++]=a[xtc++];
-        }
-	if(c!=0)
-        	printf("%-13s ",top[c].t_case);// test to print t_case
-
-        //for n_case
-        fgets(a,500,fp1);
-        int xnc=0,ync=0;
-        while(a[xnc++]!='>');
-        while(a[xnc]!='<'){
-            if(a[xnc]==' '){
-                break;
-            }
-            top[c].n_case[ync++]=a[xnc++];
-        }
-	if(c!=0)
-	        printf("  %-11s ",top[c].n_case);//test to print n_case
-
-        //for t_deaths
-        fgets(a,500,fp1);
-        int xtd=0,ytd=0;
-        while(a[xtd++]!='>');
-        while(a[xtd]!='<'){
-            if(a[xtd]==' '){
-                break;
-            }
-            top[c].t_deaths[ytd++]=a[xtd++];
-        }
-	if(c!=0)
-	        printf("  %-14s \t",top[c].t_deaths);//test to print t_deaths
-
-        //for n_deaths
-        fgets(a,500,fp1);
-        int xnd=0,ynd=0;
-        while(a[xnd++]!='>');
-        while(a[xnd]!='<'){
-            if(a[xnd]==' '){
-                break;
-            }
-            top[c].n_deaths[ynd++]=a[xnd++];
-        }
-	if(c!=0)
-        	printf("  %-12s\n",top[c].n_deaths);//test to print n_deaths
-
-        //for skipping the next few lines whos data isnt needed
-        // fgets(a,500,fp1);
-        char tr[50]="</tr>";
-        while(fgets(a,500,fp1)!=NULL){
-            if(strstr(a,tr)!=NULL){
-                break;
-            }
-        }
-        c++;
-        // printf("--------------------------------------------------------------------------------\n");
-        // printf("\n");
-    }
-    fclose(fp1);  
-    return 0;
-}
-int ind_data(){
-    //data extraction for india
-    FILE * fp1;
-    fp1=fopen("data.html","r+b");
-    char a[500];
-    fgets(a,500,fp1);
-    // puts(a);
-    char q[50]="<tbody>";
-    //skipping to the line where india will be found
-    char w[50]="India";
-    while(fgets(a,500,fp1)!=NULL){
-        if(strstr(a,q)!=NULL){
-            break;
-        }
-    }
-    while(fgets(a,500,fp1)!=NULL){
-        if(strstr(a,w)!=NULL){
-            break;
-        }
-    }
-        int p=0;
-        while(a[p++]!='>');//<td>
-        while(a[p++]!='>');//<a href>
-    // printf("the final line is --> %s\n",a); //test
-    int xi=0;
-    while(a[p]!='<'){
-        ind.name[xi++]=a[p++];
-    }
-    printf("--------------------------------------------------------------------------------\n");
-    printf("%s \n",ind.name);
-    printf("--------------------------------------------------------------------------------\n");
-    //t_case in india
-    int xitc=0,yitc=0;
-    fgets(a,500,fp1);
-    while(a[xitc++]!='>');
-    while(a[xitc]!='<'){
-        ind.t_case[yitc++]=a[xitc++];
-    }
-    printf("total cases --> %s \n",ind.t_case);
-    //for n_case in india
-    int xinc=0,yinc=0;
-    fgets(a,500,fp1);
-    while(a[xinc++]!='>');
-    while(a[xinc]!='<'){
-        ind.n_case[yinc++]=a[xinc++];
-    }
-    printf("new cases --> %s \n",ind.n_case);
-    //for total deaths in india
-    int xitd=0,yitd=0;
-    fgets(a,500,fp1);
-    while(a[xitd++]!='>');
-    while(a[xitd]!='<'){
-        ind.t_deaths[yitd++]=a[xitd++];
-    }
-    printf("total deaths --> %s \n",ind.t_deaths);
-    //for n_deaths in india
-    int xind=0,yind=0;
-    fgets(a,500,fp1);
-    while(a[xind++]!='>');
-    while(a[xind]!='<'){
-        ind.n_deaths[yind++]=a[xind++];
-    }
-    printf("new deaths --> %s \n",ind.n_deaths);
-    printf("--------------------------------------------------------------------------------\n");
-    return 0;
-}
 int print_menu(){
     int menu;
     printf("\n--------------------------------------------------------------------------------\n");
@@ -188,15 +21,73 @@ int print_menu(){
     printf("--------------------------------------------------------------------------------\n");
     return menu;
 }
-int print_menu_off(){
-    int menu;
-    printf("\n--------------------------------------------------------------------------------\n");
-    printf("\t press : \n");
-    printf("1. SYMPTOMS\n2. PRECAUTIONS\n3. RETRY \n4. QUIT\n");
-    scanf("%d",&menu);
-    printf("--------------------------------------------------------------------------------\n");
-    return menu;
+
+void india(struct data_all_country data_ind[])
+{
+	for(int i=0;i<243;i++)
+	{
+		if(strcmp(data_ind[i].country_name,"India")==0)
+		{
+			printf("--------------------------------------------------------------------------------\n");
+			printf("%s \n",data_ind[i].country_name);
+			printf("--------------------------------------------------------------------------------\n");
+			printf("total cases --> %d \nnew cases --> +%d \ntotal deaths --> %d \nnew deaths --> +%d \ntotal recovered --> %d\nnew recoverd cases --> +%d\n",data_ind[i].total_confirmed,data_ind[i].new_confirmed,data_ind[i].total_deaths,data_ind[i].new_deaths,data_ind[i].total_recovered,data_ind[i].new_recovered);
+			break;
+		}
+	}
 }
+
+void top_ten(struct data_all_country data_country[])
+{
+	char temp_name[100];
+	int temp;
+	for(int j=0;j<243;j++)
+	{
+		for(int i=0;i<242;i++)
+		{
+			if(data_country[i].total_confirmed < data_country[i+1].total_confirmed)
+			{
+				temp = data_country[i].total_confirmed;
+				data_country[i].total_confirmed = data_country[i+1].total_confirmed;
+				data_country[i+1].total_confirmed = temp;
+
+				temp = data_country[i].new_confirmed;
+				data_country[i].new_confirmed = data_country[i+1].new_confirmed;
+				data_country[i+1].new_confirmed = temp;
+
+				temp = data_country[i].total_deaths;
+				data_country[i].total_deaths = data_country[i+1].total_deaths;
+				data_country[i+1].total_deaths = temp;
+
+				temp = data_country[i].new_deaths;
+				data_country[i].new_deaths = data_country[i+1].new_deaths;
+				data_country[i+1].new_deaths = temp;
+
+				temp = data_country[i].total_recovered;
+				data_country[i].total_recovered = data_country[i+1].total_recovered;
+				data_country[i+1].total_recovered = temp;
+
+				temp = data_country[i].new_recovered;
+				data_country[i].new_recovered = data_country[i+1].new_recovered;
+				data_country[i+1].new_recovered = temp;
+
+				strcpy(temp_name,data_country[i].country_name);
+				strcpy(data_country[i].country_name,data_country[i+1].country_name);
+				strcpy(data_country[i+1].country_name,temp_name);
+			}
+		}
+	}
+	
+	for(int i=0;i<10;i++)
+	{
+		printf("--------------------------------------------------------------------------------\n");
+		printf("%d. %s \n",i+1,data_country[i].country_name);
+		printf("--------------------------------------------------------------------------------\n");
+		printf("total cases --> %d \nnew cases --> +%d \ntotal deaths --> %d \nnew deaths --> +%d \ntotal recovered --> %d\nnew recoverd cases --> +%d\n",data_country[i].total_confirmed,data_country[i].new_confirmed,data_country[i].total_deaths,data_country[i].new_deaths,data_country[i].total_recovered,data_country[i].new_recovered);
+	}	
+}
+
+
 void print_sym(){
     printf("\n\n \t SYMPTOMS : \n\n");
     printf("--------------------------------------------------------------------------------\n");
@@ -209,6 +100,7 @@ void print_sym(){
     printf("    4. Difficulty in breathing (severe cases)\n\n");
     printf("--------------------------------------------------------------------------------\n");
 }
+
 void print_info(){
     printf("\n\n\t PRECAUTIONS : \n\n");
     printf("--------------------------------------------------------------------------------\n");
@@ -227,58 +119,72 @@ void print_info(){
     printf("--------------------------------------------------------------------------------\n");
 }
 
-int main(){
-    //file download 
-    
-    CURL *curl;
-    FILE *fp;
-    int result;
-    retry:
-    fp=fopen("data.html","wb");
-    curl=curl_easy_init();
-    curl_easy_setopt(curl,CURLOPT_URL,"https://www.worldometers.info/coronavirus/");
-    curl_easy_setopt(curl,CURLOPT_WRITEDATA,fp);
-    curl_easy_setopt(curl,CURLOPT_FAILONERROR,1L);
-    result=curl_easy_perform(curl);
-    printf("\n\nWELCOME TO TRACK-KARONA\n");
-    if(result==CURLE_OK){
-        printf("\n");
+
+int main()
+{
+	CURL *curl;
+	FILE *fp;
+	int result;
+	retry:
+	fp=fopen("data.json","wb");
+	curl=curl_easy_init();
+	curl_easy_setopt(curl,CURLOPT_URL,"https://api.covid19api.com/summary");
+	curl_easy_setopt(curl,CURLOPT_WRITEDATA,fp);
+	curl_easy_setopt(curl,CURLOPT_FAILONERROR,1L);
+	result=curl_easy_perform(curl);
+	printf("\n\nWELCOME TO TRACK-KARONA\n");
+	fclose(fp);
+	curl_easy_cleanup(curl);
+
+
+	size_t i;
+	char *text;
+	struct data_all_country data_country[250];
+	json_t *root;
+	json_error_t error;
+	root = json_load_file("data.json",0,&error);
+	json_t *data, *sha, *commit, *message, *country_array,*country,*country_name;
+	json_t *total_confirmed,*new_confirmed, *total_deaths, *new_deaths, *total_recovered,*new_recovered;
+	country_array = json_object_get(root,"Countries");
+	for(int i=0;i< json_array_size(country_array);i++)
+	{
+		country = json_array_get(country_array,i);
+		if(!json_is_object(country))
+		{
+		    fprintf(stderr, "error: commit data %d is not an object\n", (int)(i + 1));
+		    json_decref(root);
+		    return 1;
+        	}
+		country_name = json_object_get(country,"Country");
+		total_confirmed = json_object_get(country,"TotalConfirmed");
+		data_country[i].total_confirmed = json_integer_value(total_confirmed);
+		new_confirmed = json_object_get(country,"NewConfirmed");
+		data_country[i].new_confirmed = json_integer_value(new_confirmed);
+		total_deaths = json_object_get(country,"TotalDeaths");
+		data_country[i].total_deaths = json_integer_value(total_deaths);
+		new_deaths = json_object_get(country,"NewDeaths");
+		data_country[i].new_deaths = json_integer_value(new_deaths);
+		total_recovered = json_object_get(country,"TotalRecovered");
+		data_country[i].total_recovered = json_integer_value(total_recovered);
+		new_recovered = json_object_get(country,"NewRecovered");
+		data_country[i].new_recovered = json_integer_value(new_recovered);
+		strcpy(data_country[i].country_name,json_string_value(country_name));
+		 
+	}
+	printf("\n");
         int option=0;
         while(option!=5){
             option = print_menu();
 	        if(option==1)
-		        top10();
+		        top_ten(data_country);
             else if(option==2)
-		        ind_data();
+		        india(data_country);
             else if(option==3)
 		        print_sym();
             else if(option==4)
                 print_info();
             else
                 break;
-    }
-    }
-    else{
-        printf("error %s \n Check your internet connection\n\n\tOFFLINE MODE \n",curl_easy_strerror(result));
-        int option=0;
-        while(option!=4){
-            option = print_menu_off();
-	        if(option==1)
-		        print_sym();
-            else if(option==2)
-		        print_info();
-            else if (option==3)
-                goto retry;   
-            else
-                break;
-    }
-    }
-    fclose(fp);
-    curl_easy_cleanup(curl);
-    //file download complete
-    printf("--------------------------------------------------------------------------------\n");
-    printf("THANK YOU\n");
-    printf("--------------------------------------------------------------------------------\n");
-    remove("data.html"); //to delete the file
-    return 0;
+	}
+	remove("data.json"); //to delete the file
 }
